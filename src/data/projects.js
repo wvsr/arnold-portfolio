fab916089a4c4a700aaf271fc5f78ca07d76a5d4
wvsr <email.wasimul@gmail.com>
2023-10-16 18:04:12 +0600

    chore(data): project list

---
+++ src/data/projects.js
@@ -0,0 +1,25 @@
+const imageRoot = '/src/assets/'
+
+export const projects = [
+  {
+    title: 'Most Profitable Products of Retail E-Commerce Company ',
+    description:
+      'El líder de TI abordó la creación de una base de datos central por sí mismo, encontrando desafíos debido a la falta de estructura y estrategia. Sin embargo, al colaborar con expertos, se seleccionaron herramientas adecuadas, se estableció un flujo de trabajo completo y se automatizó eficientemente todo el proceso. Como resultado, se logró una arquitectura bien diseñada y una mejora significativa en los informes, lo que permitió identificar con precisión las tiendas y productos de alto rendimiento.',
+    client: 'Lorem Ipsum',
+    imageUrl: `${imageRoot}RecentProject.png`,
+  },
+  {
+    title: 'Most Company ',
+    description:
+      'El líder de TI abordó la creación de una base de datos central por sí mismo, encontrando desafíos debido a la falta de estructura y estrategia. Sin embargo, al colaborar con expertos, se seleccionaron herramientas adecuadas, se estableció un flujo de trabajo completo y se automatizó eficientemente todo el proceso. Como resultado, se logró una arquitectura bien diseñada y una mejora significativa en los informes, lo que permitió identificar con precisión las tiendas y productos de alto rendimiento.',
+    client: 'Lorem Ipsum',
+    imageUrl: `${imageRoot}RecentProject.png`,
+  },
+  {
+    title: 'Most Profitable Products of Retail E-Commerce Company ',
+    description:
+      'El líder de TI abordó la creación de una base de datos central por sí mismo, encontrando desafíos debido a la falta de estructura y estrategia. Sin embargo, al colaborar con expertos, se seleccionaron herramientas adecuadas, se estableció un flujo de trabajo completo y se automatizó eficientemente todo el proceso. Como resultado, se logró una arquitectura bien diseñada y una mejora significativa en los informes, lo que permitió identificar con precisión las tiendas y productos de alto rendimiento.',
+    client: 'Lorem Ipsum',
+    imageUrl: `${imageRoot}RecentProject.png`,
+  },
+]
